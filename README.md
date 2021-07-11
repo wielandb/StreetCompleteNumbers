5d7f307f521bb2718ddab7699862ace737fece97
Wieland <29458682+wielandb@users.noreply.github.com>
2021-07-11 21:47:03 +0200

    Add Code and Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # StreetCompleteNumbers
- Python script to get the number of solved StreetComplete-Quests for a user
+
+StreetCompleteNumbers is a python script to get the number of solved [StreetComplete](https://github.com/streetcomplete/StreetComplete)-Quests for a user.
+
+## Usage
+
+Call the function StreetCompleteNumbers after importing with a [OSM](https://osm.org)-username as the parameter. The function also accepts an int as a second optional _maximum_ parameter, that if set will make the script stop getting more than ( _maximum_ * 100 ) changesets.
+
+```python
+from StreetCompleteNumbers import StreetCompleteNumbers
+StreetCompleteNumbers("maxmustermann")
+```
+
+## Example Output
+The function returns a tuple with the total number of solved quests (seen as ★ in StreetComplete) as first value and a JSON-Object containing the solved quest numbers for every quest type. 
+
+```
+(31549, {'AddMaxSpeed': 231, 'DetermineRecyclingGlass': 29, 'AddRecyclingType': 44, 'AddFootwayPartSurface': 71, 'AddCyclewayPartSurface': 68, 'AddAddressStreet': 30, 'AddHousenumber': 129, 'AddCycleway': 455, 'AddMaxWeight': 28, 'AddParkingFee': 131, 'AddParkingType': 184, 'AddParkingAccess': 209, 'AddPostboxCollectionTimes': 75, 'AddPlaygroundAccess': 85, 'AddRecyclingContainerMaterials': 58, 'AddTrafficSignalsButton': 82, 'AddTrafficSignalsVibration': 102, 'AddTrafficSignalsSound': 93, 'AddVegetarian': 75, 'AddOpeningHours': 450, 'AddPlaceName': 98, 'AddWheelchairAccessBusiness': 307, 'AddVegan': 35, 'AddBenchBackrest': 245, 'AddSidewalk': 511, 'AddStepCount': 290, 'AddHandrail': 297, 'AddStepsIncline': 259, 'AddStepsRamp': 315, 'AddPathSurface': 2983, 'AddForestLeafType': 65, 'AddRoadSurface': 987, 'AddLanes': 204, 'AddRoofShape': 4189, 'AddBuildingLevels': 6030, 'AddBuildingType': 8146, 'AddTactilePavingCrosswalk': 155, 'AddCrossingIsland': 58, 'AddCrossingType': 76, 'AddBollardType': 12, 'AddToiletsFee': 6, 'AddWheelchairAccessToilets': 9, 'AddRailwayCrossingBarrier': 18, 'AddInternetAccess': 15, 'AddGeneralFee': 5, 'AddRoadName': 58, 'AddPitchSurface': 22, 'AddBusStopShelter': 42, 'AddBenchStatusOnBusStop': 44, 'AddTactilePavingBusStop': 78, 'CheckExistence': 325, 'AddTracktype': 31, 'AddMaxHeight': 63, 'AddWayLit': 2521, 'AddBusStopLit': 40, 'AddBikeParkingType': 18, 'AddCyclewaySegregation': 83, 'AddProhibitedForPedestrians': 18, 'AddPitchLit': 19, 'AddBikeParkingFee': 1, 'AddBikeParkingCapacity': 34, 'AddClothingBinOperator': 17, 'AddFireHydrantType': 4, 'AddChargingStationOperator': 11, 'AddChargingStationCapacity': 10, 'AddBabyChangingTable': 5, 'AddBikeParkingCover': 26, 'AddBoardType': 21, 'AddBikeParkingAccess': 1, 'SpecifyShopType': 1, 'AddBarrierType': 1, 'MarkCompletedHighwayConstruction': 16, 'AddAtmOperator': 3, 'AddPowerPolesMaterial': 42, 'AddBusStopName': 3, 'AddSummitRegister': 1, 'AddBridgeStructure': 8, 'AddSport': 11, 'AddCarWashType': 4, 'CheckShopType': 5, 'MarkCompletedBuildingConstruction': 3, 'AddSuspectedOneway': 3, 'AddToiletAvailability': 1, 'AddOneway': 2, 'AddInformationToTourism': 3, 'AddWheelchairAccessOutside': 1, 'AddKerbHeight': 1, 'AddTactilePavingKerb': 1, 'AddMotorcycleParkingCover': 2, 'AddFerryAccessMotorVehicle': 1})
+```
---
+++ StreetCompleteNumbers.py
@@ -0,0 +1,109 @@
+import xml.etree.ElementTree as ET
+import requests
+import urllib
+from time import sleep
+import time
+import random
+from tqdm import tqdm
+
+"""
+Example:
+>> from StreetCompleteNumbers import StreetCompleteNumbers
+>> StreetCompleteNumbers("nutzername")
+"""
+
+
+
+def getUserChangeSets(name, maximum=0):
+    """ Maximum gibt die maximale anzahl an 100ern an, die Abgerufen werden"""
+    abfragen_anz = 1
+    closed_after = ""
+    changesets = []
+    url = "https://api.openstreetmap.org/api/0.6/changesets?display_name=" + urllib.parse.quote(name) + "&time=2001-01-01"
+    ans = requests.get(url)
+    try:
+        root = ET.fromstring(ans.text)
+    except:
+        return []
+    for child in root:
+        cs = child.attrib
+        cs["tags"] = {}
+        for tagix in range(len(child)):
+            tag = child[tagix].attrib
+            cs["tags"][tag["k"]] = tag["v"]
+        changesets.append(cs)
+    ready = False
+    try:
+        closed_after = root[len(root)-1].attrib["closed_at"]
+    except:
+        ready = True
+    while not ready:
+        url = "https://api.openstreetmap.org/api/0.6/changesets?display_name=" + urllib.parse.quote(name) + "&time=2001-01-01," + closed_after
+        ans = requests.get(url)
+        root = ET.fromstring(ans.text)
+        if len(root) < 100:
+            ready = True
+        for child in root:
+            cs = child.attrib
+            cs["tags"] = {}
+            for tagix in range(len(child)):
+                tag = child[tagix].attrib
+                cs["tags"][tag["k"]] = tag["v"]
+            changesets.append(cs)
+        try:
+            closed_after = root[len(root)-1].attrib["closed_at"]
+        except:
+            ready = True
+            break
+        abfragen_anz += 1
+        if (abfragen_anz > maximum and maximum != 0):
+            break
+        sleep(0.5)
+    changesets = cleanChangeSets(changesets)
+    return changesets 
+
+
+def cleanChangeSets(changesets):
+    newcss = []
+    for cs in changesets:
+        schonda = False
+        for ncs in newcss:
+            if ncs["id"] == cs["id"]:
+                schonda = True
+        if not schonda:
+            newcss.append(cs)
+    return newcss
+
+def ChangeSetsToStreetCompleteNumbers(changesets):
+    quests = {}
+    total = 0
+    for changeset in changesets:
+        if "StreetComplete:quest_type" in changeset["tags"]:
+            if changeset["tags"]["StreetComplete:quest_type"] in quests:
+                quests[changeset["tags"]["StreetComplete:quest_type"]] += int(changeset["changes_count"])
+            else:
+                quests[changeset["tags"]["StreetComplete:quest_type"]] = int(changeset["changes_count"])
+    for q in quests:
+        total += quests[q]
+    print(total)
+    return total, quests
+
+def StreetCompleteNumbers(user, maximum=200):
+    print(ChangeSetsToStreetCompleteNumbers(cleanChangeSets(getUserChangeSets(schlechtesErsetzen(user), maximum))))
+
+def getIDhaeufig(changesets):
+    ids = {}
+    for changeset in changesets:
+        if changeset["id"] in ids:
+            ids[changeset["id"]] += 1
+        else:
+            ids[changeset["id"]] = 1
+    return ids
+
+def schlechtesErsetzen(text):
+    text = text.replace("Ã¼", "ü")
+    text = text.replace("Ã¤", "ä")
+    text = text.replace("Ã¶", "ö")
+    #text = text.replace("Ã¼", "ü")
+    return text
+
